a904da6c20a5f70d3ba9abb4030d707f34ed4eba
Alisimran95 <alisimranem@code.edu.az>
2022-01-07 22:09:19 +0400

    parallax  section completed

--- js/index.js
+++ js/index.js
@@ -6,3 +6,25 @@ menuIcon.addEventListener("click",()=>{
     navLinks.classList.toggle('active');
 
 })
+
+// Parallax counter 
+const counters  = document.querySelectorAll(".counter");
+const countSpeed = 1;
+
+counters.forEach(c=>{
+    const boostCount = ()=>{
+        const dataCount = +c.getAttribute("data-count");
+        const count =  + c.innerText;
+
+        const increment = dataCount / countSpeed;
+
+        if (count<dataCount) {
+            c.innerText = count + increment;
+            setTimeout(boostCount,10000);
+        }
+        else{
+            c.innerText = dataCount;
+        }
+    }
+    boostCount();
+})
