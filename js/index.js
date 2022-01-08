35db067a842badc2e97956019bdf3349ce69467c
Alisimran95 <alisimranem@code.edu.az>
2022-01-08 04:16:47 +0400

    teams section created

--- js/index.js
+++ js/index.js
@@ -8,23 +8,23 @@ menuIcon.addEventListener("click",()=>{
 })
 
 // Parallax counter 
-const counters  = document.querySelectorAll(".counter");
-const countSpeed = 1;
+// const counters  = document.querySelectorAll(".counter");
+// const countSpeed = 1;
 
-counters.forEach(c=>{
-    const boostCount = ()=>{
-        const dataCount = +c.getAttribute("data-count");
-        const count =  + c.innerText;
+// counters.forEach(c=>{
+//     const boostCount = ()=>{
+//         const dataCount = +c.getAttribute("data-count");
+//         const count =  + c.innerText;
 
-        const increment = dataCount / countSpeed;
+//         const increment = dataCount / countSpeed;
 
-        if (count<dataCount) {
-            c.innerText = count + increment;
-            setTimeout(boostCount,10000);
-        }
-        else{
-            c.innerText = dataCount;
-        }
-    }
-    boostCount();
-})
+//         if (count<dataCount) {
+//             c.innerText = count + increment;
+//             setTimeout(boostCount,10000);
+//         }
+//         else{
+//             c.innerText = dataCount;
+//         }
+//     }
+//     boostCount();
+// })
