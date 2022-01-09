95ea9e7138267729d752c46cf890d1cd7eff81b3
Alisimran95 <alisimranem@code.edu.az>
2022-01-09 21:41:38 +0400

    faq section created

--- js/index.js
+++ js/index.js
@@ -1,11 +1,24 @@
 // Navbar dropdown click
 const menuIcon = document.querySelector(".menu-icon");
-const navLinks =document.querySelector(".nav-links");
+const navLinks = document.querySelector(".nav-links");
 
-menuIcon.addEventListener("click",()=>{
+menuIcon.addEventListener("click", () => {
     navLinks.classList.toggle('active');
 
 })
+
+// Accordion FAQ section start
+
+
+document.querySelectorAll(".accordion-question").forEach(a => {
+    a.addEventListener("click", () => {
+        if (a.classList.contains("accordion-question")) {
+            a.nextElementSibling.classList.toggle("active");
+        }
+    })
+})
+
+// Accordion Faq section ends
 
 // Parallax counter 
 // const counters  = document.querySelectorAll(".counter");
